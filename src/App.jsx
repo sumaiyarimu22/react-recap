6bf78906607d1287bf3a0575b6f4010907638e9d
Sumaiya Rimu <sumaiyarimu22@gmail.com>
2023-08-16 21:53:42 +0600

    localState useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,14 @@
-import { useState, useCallback, useMemo } from "react";
-import Button from "./components/Button";
-import ShowCount from "./components/ShowCount";
-import Title from "./components/Title";
+// import ComplexCounter from "./components/ComplexCounter";
 
-export default function App() {
-  const [count1, setcount1] = useState(0);
-  const [count2, setcount2] = useState(0);
+import SimpleCounter from "./components/SimpleCounter";
 
-  const incrementByOne = useCallback(() => {
-    //function memorize kore rakhe
-    setcount1((prevCount) => prevCount + 1);
-  }, []);
-
-  const incrementByFive = useCallback(() => {
-    setcount2((preCount) => preCount + 5);
-  }, []);
-
-  const isEventOrOdd = useMemo(() => {
-    // function arte return value memorize kore
-    let i = 0;
-    while (i < 1000) {
-      i = i + 1;
-    }
-    return count1 % 2 === 0;
-  }, [count1]);
-
+const App = () => {
   return (
     <div>
-      <Title />
-      <ShowCount count={count1} title="Counter 1" />
-      <span>{isEventOrOdd ? "even" : "odd"}</span>
-      <Button handleClick={incrementByOne}>Increment by one</Button>
-      <hr />
-      <ShowCount count={count2} title="counter 2" />
-      <Button handleClick={incrementByFive}>Increment by five</Button>
+      {/* <ComplexCounter /> */}
+      <SimpleCounter />
     </div>
   );
-}
+};
+
+export default App;
