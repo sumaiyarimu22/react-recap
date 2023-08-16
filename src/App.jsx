e5ca92a01210b46935ea214266eed194417c6ffd
Sumaiya Rimu <sumaiyarimu22@gmail.com>
2023-08-16 23:47:48 +0600

    diffrence between useReducer & useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,9 @@
-import React, { useReducer } from "react";
-import ComponantA from "./Components/ComponantA";
-
-// eslint-disable-next-line react-refresh/only-export-components
-export const counterContext = React.createContext();
-
-const initialState = 0;
-
-const reducer = (state, action) => {
-  //state,action
-  switch (action) {
-    case "increment":
-      return state + 1;
-    case "decrement":
-      return state - 1;
-    default:
-      return state;
-  }
-};
+import GetPost2 from "./components/GetPost2";
 
 const App = () => {
-  const [count, dispatch] = useReducer(reducer, initialState);
   return (
     <div>
-      <div>Count:{count}</div>
-      <counterContext.Provider value={{ countDispatch: dispatch }}>
-        <ComponantA />
-      </counterContext.Provider>
+      <GetPost2 />
     </div>
   );
 };
