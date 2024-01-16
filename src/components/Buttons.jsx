f01e7363f985765678c1ee9c0a12ce7aacb94211
sumaiyarimu22 <sumaiyarimu22@gmail.com>
2024-01-17 00:08:00 +0600

    Ecommerce-search & filtering add

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,5 +1,9 @@
-const Buttons = () => {
-  return <div>Buttons</div>;
+const Buttons = ({ onClickHandler, value, title }) => {
+  return (
+    <button onClick={onClickHandler} value={value} className='btns'>
+      {title}
+    </button>
+  );
 };
 
 export default Buttons;
