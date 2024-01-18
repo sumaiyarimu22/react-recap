08ee5019ef261513863fdf3cfa99bbb3b07d77c0
sumaiyarimu22 <sumaiyarimu22@gmail.com>
2024-01-18 13:17:48 +0600

    Trending Books done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
+import BookBoard from "./components/BookFinder/BookBoard";
 import Footer from "./components/Footer";
-import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-import TaskBoard from "./components/Tasks/TaskBoard";
 
 const App = () => {
   return (
-    <div className='bg-[#191D26] font-[Inter] text-white '>
+    <div>
       <Navbar />
-      <Hero />
-      <div className='container mx-auto'>
-        <TaskBoard />
-        <Footer />
-      </div>
+      <BookBoard />
+      <Footer />
     </div>
   );
 };
--- src/components/Tasks/NoTask.jsx
+++
@@ -1,5 +0,0 @@
-const NoTask = () => {
-  return <h2>No Task Here</h2>;
-};
-
-export default NoTask;
